1a131b5348df3aa26ef77b17054342da3ebc85fa
Josef Forkman <strand.vatten@outlook.com>
2024-03-19 15:52:59 +0100

    shadcn Typography

---
+++ src/components/ui/Typography/TypographyLarge.tsx
@@ -0,0 +1,4 @@
+export function TypographyLarge({ children }: { children: React.ReactNode }) {
+    return <div className="text-lg font-semibold">{children}</div>
+}
+
---
+++ src/components/ui/Typography/TypographyLead.tsx
@@ -0,0 +1,7 @@
+export function TypographyLead({ children }: { children: React.ReactNode }) {
+    return (
+        <p className="text-sm text-muted-foreground">
+            {children}
+        </p>
+    )
+}
