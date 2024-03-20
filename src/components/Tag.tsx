54ad86fd6736fd1704dc2cbfe362b3300f424c4b
Josef Forkman <strand.vatten@outlook.com>
2024-03-20 15:34:26 +0100

    Update gap size in Tag component

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -3,7 +3,7 @@ type Tag = { id: number, href: string, title: string }
 
 export default function Tags({ Tags }: { Tags: Tag[] }) {
     return (
-        <div className="flex gap-2">
+        <div className="flex gap-1">
             {Tags.map((tag) => (
                 <Link key={tag.id} href={tag.href} className="bg-slate-500 text-white py-2 px-4 rounded-full hover:bg-slate-600 focus-visible:bg-slate-600 focus-visible:outline-1 focus-visible:outline-offset-4">{tag.title}</Link>
             ))}
