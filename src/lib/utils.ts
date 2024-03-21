353b106eabd3aa9f27db4c637507cc84cff62a6c
Josef Forkman <strand.vatten@outlook.com>
2024-03-21 11:45:18 +0100

    add a simpel getInitials function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
+
+export const getInitials = (name: string) => {
+    const parts = name.split(' ')
+    let initials = ''
+    for (let i = 0; i < parts.length; i++) {
+        if (parts[i].length > 0 && parts[i] !== '') {
+            initials += parts[i][0]
+        }
+    }
+    return initials
+}
