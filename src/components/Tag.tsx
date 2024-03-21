20a50d7c3854e0fe9551b4d555609e9b7314e5c1
Josef Forkman <strand.vatten@outlook.com>
2024-03-21 10:35:15 +0100

    Update Tag.tsx

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -3,10 +3,12 @@ type Tag = { id: number, href: string, title: string }
 
 export default function Tags({ Tags }: { Tags: Tag[] }) {
     return (
-        <div className="flex gap-1">
+        <ul className="flex gap-1">
             {Tags.map((tag) => (
-                <Link key={tag.id} href={tag.href} className="bg-slate-500 text-white py-2 px-4 rounded-full hover:bg-slate-600 focus-visible:bg-slate-600 focus-visible:outline-1 focus-visible:outline-offset-4">{tag.title}</Link>
+                <li>
+                    <Link key={tag.id} href={tag.href} className="inline-block text-xs bg-slate-500 text-white py-1 px-2 rounded-full hover:bg-slate-600 focus-visible:bg-slate-600 focus-visible:outline-1 focus-visible:outline-offset-4">{tag.title}</Link>
+                </li>
             ))}
-        </div>
+        </ul>
     )
 }
