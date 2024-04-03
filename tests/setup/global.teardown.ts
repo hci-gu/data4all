9e27c141c8da44264da78c4bf626a4cd0d405a8d
Josef Forkman <strand.vatten@outlook.com>
2024-04-03 14:34:08 +0200

    Refactor global teardown setup function to filter and delete datasets with relations containing 'test' in the title

--- tests/setup/global.teardown.ts
+++ tests/setup/global.teardown.ts
@@ -2,7 +2,7 @@ import { test as setup } from '@playwright/test'
 import PocketBase from 'pocketbase'
 const pb = new PocketBase('http://localhost:8090')
 
-setup('setup', async ({ page, request, context }) => {
+setup('setup', async () => {
     const users = await pb.collection('users').getFullList()
 
     for (const user of users) {
@@ -12,6 +12,17 @@ setup('setup', async ({ page, request, context }) => {
     }
 
     const datasets = await pb.collection('dataset').getFullList()
+
+    const datasetWithRelations = datasets.filter(
+        (dataset) => dataset.related_datasets.length > 0
+    )
+
+    for (const dataset of datasetWithRelations) {
+        if (dataset.title.includes('test')) {
+            await pb.collection('dataset').delete(dataset.id)
+            datasets.splice(datasets.indexOf(dataset), 1)
+        }
+    }
 
     for (const dataset of datasets) {
         if (dataset.title.includes('test')) {
