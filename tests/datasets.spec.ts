2f24f16319cd97d2733a9c51a155ab61e5066350
Josef Forkman <strand.vatten@outlook.com>
2024-04-03 14:35:39 +0200

    creates three datasets with relationships to the last dataset

--- tests/datasets.spec.ts
+++ tests/datasets.spec.ts
@@ -5,6 +5,20 @@ import {
     loggedInUser,
 } from './setup/utils'
 
+const searchTerms = [
+    'unique test title 1',
+    'unique test title 2',
+    'unique test title related 3',
+]
+
+test.describe.configure({ mode: 'serial' })
+
+test.beforeAll(async () => {
+    const dataset1 = await createDataset(searchTerms[0])
+    const dataset2 = await createDataset(searchTerms[1])
+    await createDatasetWithRelation(searchTerms[2], [dataset1, dataset2])
+})
+
 test.describe('Datasets page', () => {
     test.describe('Logged in user', () => {
         test.beforeEach(async ({ page, request, context }) => {
@@ -12,10 +26,9 @@ test.describe('Datasets page', () => {
         })
 
         test('Can reach the datasets page', async ({ page }) => {
-            const dataset = await createDataset('test title')
-            await page.goto(`/dataset/${dataset.title}`)
+            await page.goto(`/dataset/${searchTerms[0]}`)
             await expect(page.getByRole('heading', { level: 1 })).toHaveText(
-                dataset.title
+                searchTerms[0]
             )
         })
         test('Gets a 404 page when going to a non existent dataset', async ({
@@ -28,29 +41,20 @@ test.describe('Datasets page', () => {
         })
 
         test('Can see two attached related datasets', async ({ page }) => {
-            const dataset1 = await createDataset('test title 1')
-            const dataset2 = await createDataset('test title 2')
-            const dataset = await createDatasetWithRelation('test title', [
-                dataset1,
-                dataset2,
-            ])
-
-            await page.goto(`/dataset/${dataset.title}`)
-            await page.click(`text=${dataset1.title}`)
+            await page.goto(`/dataset/${searchTerms[2]}`)
+            await page.click(`text=${searchTerms[0]}`)
             await expect(page.getByRole('heading', { level: 1 })).toHaveText(
-                dataset1.title
+                searchTerms[0]
             )
 
             await page.goBack()
-            await page.click(`text=${dataset2.title}`)
+            await page.click(`text=${searchTerms[1]}`)
             await expect(page.getByRole('heading', { level: 1 })).toHaveText(
-                dataset2.title
+                searchTerms[1]
             )
         })
         test('Can not see related datasets', async ({ page }) => {
-            const dataset = await createDataset('test title')
-
-            await page.goto(`/dataset/${dataset.title}`)
+            await page.goto(`/dataset/${searchTerms[0]}`)
             await expect(
                 page.getByText('Det finns inga relaterade dataset')
             ).toBeVisible()
@@ -61,8 +65,8 @@ test.describe('Datasets page', () => {
         test('Can not reach the datasets page and gets redirected to the login page', async ({
             page,
         }) => {
-            const dataset = await createDataset('test title')
-            await page.goto(`/dataset/${dataset.title}`)
+            // const dataset = await createDataset('test title')
+            await page.goto(`/dataset/${searchTerms[0]}`)
 
             await expect(page).toHaveURL(`/logga-in`)
         })
