06e1559b2d6645c300ff5bd066c7d61217cbae85
JosefForkman <strand.vatten@outlook.com>
2024-04-10 09:13:56 +0100

    Fix image on login and signup page

--- src/app/skapa-konto/page.tsx
+++ src/app/skapa-konto/page.tsx
@@ -6,7 +6,7 @@ import { SignUp } from '@/components/auth'
 function SignUpPage() {
     return (
         <main className="grid h-screen sm:grid-cols-2">
-            <div className="hidden items-center bg-black px-40 sm:flex">
+            <div className="hidden items-center bg-black sm:px-12 md:px-24 xl:px-40 sm:flex">
                 <Image src={absurd} priority={true} alt="" />
             </div>
             <div className="flex flex-col justify-center gap-12 px-4 sm:mx-auto">
