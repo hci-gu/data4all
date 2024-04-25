0943a9f0005904dd6a447a2acc181a2b7fae2f6c
Styrbjörn Nordberg <styris.n@gmail.com>
2024-04-25 14:10:40 +0200

    seed arrays moved to json file & made seed clearer

--- seed.js
+++ seed.js
@@ -1,4 +1,5 @@
 import PocketBase from 'pocketbase'
+import fs from 'fs/promises'
 
 const stringWithHyphen = (text) => {
     return text.toLowerCase().replaceAll(' ', '-')
@@ -9,240 +10,64 @@ const getRandomTag = (tags) => {
 }
 
 ;(async () => {
+    const jsonData = await fs.readFile('seedData.json', 'utf-8')
+    const { tags, datasets, users } = JSON.parse(jsonData)
+
     const pb = new PocketBase('http://localhost:8090')
     await pb.admins.authWithPassword('admin@email.com', 'password123')
 
-    const newTestTagsArray = [
-        'Energy',
-        'Finance',
-        'Weather',
-        'Web Analytics',
-        'Environmental',
-        'Sales',
-        'Economics',
-        'Social Media',
-        'Transportation',
-        'Employment',
-        'CRM',
-        'Economic Indicators',
-        'Climate',
-        'Market Analysis',
-        'Demographics',
-    ]
+    try {
+        await deleteExistingData(pb, 'dataset', datasets)
+        await deleteExistingData(pb, 'users', users, 'email')
+        await deleteExistingData(pb, 'tag', tags, 'name')
 
-    const newTestRecordsArray = [
-        {
-            title: 'Electricity Consumption',
-            description:
-                'Time series data representing electricity consumption in kilowatt-hours (kWh) over a period of time.',
-        },
-        {
-            title: 'Stock Prices',
-            description:
-                "Historical stock prices of a particular company's shares over time.",
-        },
-        {
-            title: 'Temperature Readings',
-            description:
-                'Time series data representing temperature readings in Celsius or Fahrenheit over time.',
-        },
-        {
-            title: 'Website Traffic',
-            description:
-                'Time series data representing the number of visitors to a website over time.',
-        },
-        {
-            title: 'Air Quality Index',
-            description:
-                'Time series data representing the Air Quality Index (AQI) over time.',
-        },
-        {
-            title: 'Sales Revenue',
-            description:
-                'Time series data representing sales revenue of a company over a period of time.',
-        },
-        {
-            title: 'Exchange Rates',
-            description:
-                'Time series data representing exchange rates between two currencies over time.',
-        },
-        {
-            title: 'Social Media Mentions',
-            description:
-                'Time series data representing the number of mentions or interactions on a social media platform over time.',
-        },
-        {
-            title: 'Traffic Congestion',
-            description:
-                'Time series data representing the level of traffic congestion in a city over time.',
-        },
-        {
-            title: 'Energy Production',
-            description:
-                'Time series data representing energy production (e.g., solar, wind, hydro) over time.',
-        },
-        {
-            title: 'Unemployment Rate',
-            description:
-                'Time series data representing the unemployment rate in a region over time.',
-        },
-        {
-            title: 'Customer Churn',
-            description:
-                'Time series data representing the rate of customer churn (i.e., customer attrition) over time.',
-        },
-        {
-            title: 'GDP Growth',
-            description:
-                'Time series data representing the Gross Domestic Product (GDP) growth rate of a country over time.',
-        },
-        {
-            title: 'Rainfall',
-            description:
-                'Time series data representing the amount of rainfall in millimeters over time.',
-        },
-    ]
+        const createdTags = await seedData(pb, 'tag', tags)
 
-    const newTestUsers = [
-        {
-            name: 'Suzuka Nakamoto',
-            email: 'Suzuka_Nakamoto@kungsbacka.com',
-            role: 'User',
-            password: 'password123',
-        },
-        {
-            name: 'Moa Kikuchi',
-            email: 'Moa_Kikuchi@kungsbacka.com',
-            role: 'User',
-            password: 'password123',
-        },
-        {
-            name: 'Momoko Okazaki',
-            email: 'Momoko_Okazaki@kungsbacka.com',
-            role: 'User',
-            password: 'password123',
-        },
-        {
-            name: 'John Smith',
-            email: 'john_smith@kungsbacka.com',
-            role: 'User',
-            password: 'password123',
-        },
-        {
-            name: 'Maria Rodriguez',
-            email: 'maria_rodriguez@kungsbacka.com',
-            role: 'User',
-            password: 'password123',
-        },
-        {
-            name: 'Chen Wei',
-            email: 'chen_wei@kungsbacka.com',
-            role: 'User',
-            password: 'password123',
-        },
-        {
-            name: 'Emily Johnson',
-            email: 'emily_johnson@kungsbacka.com',
-            role: 'User',
-            password: 'password123',
-        },
-        {
-            name: 'Muhammad Khan',
-            email: 'muhammad_khan@kungsbacka.com',
-            role: 'User',
-            password: 'password123',
-        },
-        {
-            name: 'Sophie Martin',
-            email: 'sophie_martin@kungsbacka.com',
-            role: 'User',
-            password: 'password123',
-        },
-        {
-            name: 'Kim Sung',
-            email: 'kim_sung@kungsbacka.com',
-            role: 'User',
-            password: 'password123',
-        },
-    ]
+        await seedData(pb, 'users', users)
 
-    const testdatasets = await pb.collection('dataset').getFullList()
-    if (testdatasets.length > 0) {
-        for (let index = 0; index < testdatasets.length; index++) {
-            for (let j = 0; j < newTestRecordsArray.length; j++) {
-                const testRecord = newTestRecordsArray[j]
-                if (testdatasets[index].title.includes(testRecord.title)) {
-                    await pb
-                        .collection('dataset')
-                        .delete(testdatasets[index].id)
-                }
+        await seedDataset(pb, 'dataset', datasets, createdTags)
+    } catch (error) {
+        console.error('There was an error: ', error)
+    }
+})()
+
+async function deleteExistingData(
+    pb,
+    collectionName,
+    data,
+    identifier = 'title'
+) {
+    const existingData = await pb.collection(collectionName).getFullList()
+    for (const existingItem of existingData) {
+        for (const newItem of data) {
+            if (existingItem[identifier] === newItem[identifier]) {
+                await pb.collection(collectionName).delete(existingItem.id)
+                break
             }
         }
     }
+}
 
-    const testTags = await pb.collection('tag').getFullList()
-    for (let index = 0; index < testTags.length; index++) {
-        if (newTestTagsArray.includes(testTags[index].name)) {
-            await pb.collection('tag').delete(testTags[index].id)
-        }
+async function seedData(pb, collectionName, data) {
+    const items = []
+    for (const itemData of data) {
+        const newItem = await pb
+            .collection(collectionName)
+            .create(itemData, { $autoCancel: false })
+        items.push(newItem)
     }
+    return items
+}
 
-    let tags = []
-    for (let i = 0; i < newTestTagsArray.length; i++) {
-        const tag = newTestTagsArray[i]
-        tags.push(
-            await pb.collection('tag').create(
-                {
-                    name: tag,
-                },
-                { $autoCancel: false }
-            )
-        )
-    }
-    await Promise.all(tags)
-
-    for (let i = 0; i < newTestRecordsArray.length; i++) {
-        const record = newTestRecordsArray[i]
-        await pb.collection('dataset').create(
+async function seedDataset(pb, collectionName, data, tags) {
+    for (const itemData of data) {
+        await pb.collection(collectionName).create(
             {
-                ...record,
-                slug: stringWithHyphen(record.title),
+                ...itemData,
+                slug: stringWithHyphen(itemData.title),
                 tag: [getRandomTag(tags)],
             },
             { $autoCancel: false }
         )
     }
-
-    const allusers = await pb.collection('users').getFullList()
-    if (allusers.length > 0) {
-        for (let index = 0; index < allusers.length; index++) {
-            for (let j = 0; j < newTestUsers.length; j++) {
-                const testUser = newTestUsers[j]
-                if (allusers[index].email.includes(testUser.email)) {
-                    await pb.collection('users').delete(allusers[index].id)
-                }
-            }
-        }
-    }
-
-    let testUsers = []
-    for (let index = 0; index < newTestUsers.length; index++) {
-        const user = newTestUsers[index]
-        testUsers.push(
-            await pb.collection('users').create(
-                {
-                    name: user.name,
-                    email: user.email,
-                    role: user.role,
-                    password: user.password,
-                    passwordConfirm: user.password,
-                },
-                {
-                    $autoCancel: false,
-                }
-            )
-        )
-    }
-
-    await Promise.all(testUsers)
-})()
+}
