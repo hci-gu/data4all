e4a1ef6793e75e50d27a5309b3e9167e3e8c7fbe
Josef Forkman <strand.vatten@outlook.com>
2024-04-25 10:50:48 +0200

    Update global.teardown.ts

--- tests/setup/global.teardown.ts
+++ tests/setup/global.teardown.ts
@@ -3,38 +3,38 @@ import PocketBase from 'pocketbase'
 const pb = new PocketBase('http://localhost:8090')
 
 setup('setup', async () => {
-    // await pb.admins.authWithPassword('admin@email.com', 'password123')
-    // const datasets = await pb.collection('dataset').getFullList()
-    // const events = await pb.collection('events').getFullList()
+    await pb.admins.authWithPassword('admin@email.com', 'password123')
+    const datasets = await pb.collection('dataset').getFullList()
+    const events = await pb.collection('events').getFullList()
 
-    // const users = await pb.collection('users').getFullList()
+    const users = await pb.collection('users').getFullList()
 
-    // const datasetWithRelations = datasets.filter(
-    //     (dataset) => dataset.related_datasets.length > 0
-    // )
+    const datasetWithRelations = datasets.filter(
+        (dataset) => dataset.related_datasets.length > 0
+    )
 
-    // for (const dataset of datasetWithRelations) {
-    //     if (dataset.title.includes('test')) {
-    //         await pb.collection('dataset').delete(dataset.id)
-    //         datasets.splice(datasets.indexOf(dataset), 1)
-    //     }
-    // }
+    for (const dataset of datasetWithRelations) {
+        if (dataset.title.includes('test')) {
+            await pb.collection('dataset').delete(dataset.id)
+            datasets.splice(datasets.indexOf(dataset), 1)
+        }
+    }
 
-    // for (const dataset of datasets) {
-    //     if (dataset.title.includes('test')) {
-    //         await pb.collection('dataset').delete(dataset.id)
-    //     }
-    // }
+    for (const dataset of datasets) {
+        if (dataset.title.includes('test')) {
+            await pb.collection('dataset').delete(dataset.id)
+        }
+    }
 
-    // for (const event of events) {
-    //     if (event.content === 'test' || event.content.includes('tester')) {
-    //         await pb.collection('events').delete(event.id)
-    //     }
-    // }
+    for (const event of events) {
+        if (event.content === 'test' || event.content.includes('tester')) {
+            await pb.collection('events').delete(event.id)
+        }
+    }
 
-    // for (const user of users) {
-    //     if (user.name.includes('tester')) {
-    //         await pb.collection('users').delete(user.id)
-    //     }
-    // }
+    for (const user of users) {
+        if (user.name.includes('tester')) {
+            await pb.collection('users').delete(user.id)
+        }
+    }
 })
