ec65ea2335d370a2901dc4f92f7e06c5be85d1b6
Styrbjörn Nordberg <styris.n@gmail.com>
2024-05-28 13:33:41 +0200

    index page feed styling

--- src/components/feedItem.tsx
+++ src/components/feedItem.tsx
@@ -19,7 +19,12 @@ export default function FeedItem({ event }: { event: EventFeedItem }) {
                     <div className="flex flex-col items-start">
                         <p className="mt-1 text-xs">
                             <b>{event.userName}</b> kommenterade i
-                            <b> {event.datasetTitle}</b>
+                            <b>
+                                {' '}
+                                <Link href={'/'}>
+                                    {event.datasetTitle}
+                                </Link>{' '}
+                            </b>
                         </p>
                     </div>
                     <div className="flex w-full flex-col gap-1">
@@ -64,22 +69,28 @@ export default function FeedItem({ event }: { event: EventFeedItem }) {
     }
 
     return (
-        <li className="flex gap-2">
+        <li className="flex w-full gap-1">
             <Avatar className="h-6 w-6">
                 <AvatarFallback className="text-[0.5625rem]">
                     {getInitials(event.userName)}
                 </AvatarFallback>
             </Avatar>
-            <div className="mt-1 flex flex-col gap-1">
-                <div
-                    className="text-xs"
-                    dangerouslySetInnerHTML={{
-                        __html: `${event.content} i <b>${event.datasetTitle}</b>`,
-                    }}
-                />
-                <time className="text-xs font-bold">
-                    {moment(event.created).fromNow()}
-                </time>
+            <div className="flex w-full flex-col gap-1">
+                <div className="flex flex-col items-start">
+                    <p className="mt-1 text-xs">
+                        <b>{event.userName}</b> Föreslod en dataägare i
+                        <b>
+                            {' '}
+                            <Link href={'/'}>{event.datasetTitle}</Link>{' '}
+                        </b>
+                    </p>
+                </div>
+                <div className="flex w-full flex-col gap-1">
+                    <SlateComment event={event} />
+                    <time className="text-xs font-bold">
+                        {moment(event.created).fromNow()}
+                    </time>
+                </div>
             </div>
         </li>
     )
