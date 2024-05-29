65f2e8cb3e72e2b3e410ec4a27368d19fdd1b819
Josef Forkman <strand.vatten@outlook.com>
2024-05-29 09:14:30 +0200

    remove eventForm component

--- src/components/dataset/index.ts
+++ src/components/dataset/index.ts
@@ -1,7 +1,6 @@
 import Datasets from './datasets'
 import ActivityFlow from './activityFlow'
-import EventForm from './eventForm'
 import Comment from './comment'
 import SuggestDataOwner from './suggestDataOwner'
 
-export { Datasets, ActivityFlow, EventForm, Comment, SuggestDataOwner }
+export { Datasets, ActivityFlow, Comment, SuggestDataOwner }
